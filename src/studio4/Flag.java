39147a04bba487dd9a3d98c73b5a8c7eb09e05db
jonas <jonas@10.232.179.83>
2022-10-13 14:10:38 -0500

    smile

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,26 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		boolean filled;
+		double x;
+		double y;
+		double borderX;
+		double borderY;
+		
+		Color purple = new Color(220, 77, 244);
+		StdDraw.setPenColor(purple);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.3);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.2);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.4, 0.1, 0.03);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.4, 0.6, 0.02, 0.02);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.6, 0.6, 0.02, 0.02);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,36 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt(); 
+		int blueComponent = in.nextInt(); 
+		
+		boolean filled  = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double borderX = in.nextDouble();
+		double borderY = in.nextDouble();
+		//double x = in.nextDouble(); 
+		//System.out.println(x);
+		if (shapeType.equals("rectangle")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.filledRectangle(x, y, borderX, borderY);
+
+		} else if (shapeType.equals("ellipse")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.filledEllipse(x, y, borderX, borderY);
+		} else if (shapeType.equals("triangle")) {
+//			double borderZ = in.nextDouble();
+//			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+//			StdDraw.filledTriangle(x, y, borderX, borderY, borderZ);
+			System.out.println("sorry we can't print triangles");
+		}
+//		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+//		StdDraw.filledEllipse(x, y, borderX, borderY);
+//		
+		
+		 
 	}
 }
